6439f3b83d684d42b06005e85a1ef50e81a2453a
awaissaddiqui <awaissaddiqui143@gmail.com>
2023-08-02 19:48:46 +0500

    2. About section

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import pages 
+import AboutUs from './pages/AboutUs'
+
+const App = () => {
+  return (
+     <AboutUs/>
+     )
+}
+
+export default App
---
+++ src/components/AboutSection.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import home1 from "./images/home1.jpeg"
+const AboutSection = () => {
+  return (
+    <div>
+    <div className="description">
+      <div className="title">
+        <div className="hide">
+          <h2>We work to make </h2>
+        </div>
+        <div className="hide">
+          <h2>your <span>dreams</span> come </h2>
+        </div>
+        <div className="hide">
+          <h2>true</h2>
+        </div>
+      </div>
+      <p>We are providing an amazing skills .
+         Please contact us for professional work</p>
+         <button>Contact Us</button>
+         <div className="image">
+      <img src={home1} alt="home1" />
+         </div>
+    </div>
+    </div>
+  )
+}
+
+export default AboutSection
