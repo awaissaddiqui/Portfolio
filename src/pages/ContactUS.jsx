e0079d81f395c50e30f8272bff6e10dfaae0f845
awaissaddiqui <awaissaddiqui143@gmail.com>
2023-08-11 23:24:23 +0500

    Contact US page added and styled

--- src/pages/ContactUS.jsx
+++ src/pages/ContactUS.jsx
@@ -1,12 +1,23 @@
-import React,{useRef} from 'react'
+import React,{useRef, useState} from 'react'
 import emailjs from "emailjs-com"
+//import svgs
+import phone from "../images/inputAssets/phone.svg"
+import emailPic from "../images/inputAssets/email.svg"
+import location from "../images/inputAssets/location.svg"
 // Import Animation
 import { motion } from "framer-motion";
 import { pageAnimation, titleAnimation } from '../Animations/PageAnimation';
-import {ContactStyle, Title, Hide} from "../styles/contactStyle";
+import {ContactStyle, Title, Hide, Form,StyledInput,StyledText,StyledButton} from "../styles/contactStyle";
 const ContactUS = () => {
   const form = useRef();
-const sendEmail=(e)=>{
+  const [name, setName]=useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("")
+  const [message,  setMessage]= useState('');
+
+
+
+  const sendEmail=(e)=>{
   e.preventDefault();  
   emailjs.sendForm(
     process.env.REACT_APP_SERVICE_ID,
@@ -33,14 +44,17 @@ const sendEmail=(e)=>{
       <Title>
           <motion.h2 variants={titleAnimation}>Get In Touch</motion.h2>
       </Title>
+        <div className='phone'><span><img src={phone} alt="phone" /></span>Phone: <span>+92343-9856501</span> </div>
+        <div className='email'><span><img src={emailPic} alt="email" /></span>Email: <span>awaissaddiqui143@gmail.com</span></div> 
+        <div className='address'><span><img src={location} alt="address" /></span>Address: <span>Islamabad, Pakistan</span></div>  
         </Hide>
-      <form ref={form} onSubmit={sendEmail} >
-        <input type="text" name="name" id="name" placeholder='Name' required />
-        <input type="email" name="email" id="email" placeholder='Email' required />
-        <input type="text" name="subject" id="subject" placeholder='Subject' required />
-        <textarea name='message' id='message' placeholder='Message' required/>
-        <input type="submit" value="send" />
-      </form>
+      <Form ref={form} onSubmit={sendEmail} id="form" >
+        <StyledInput type="text" name="name" id="name" placeholder='Name' required />
+        <StyledInput type="email" name="email" id="email" placeholder='Email' required />
+        <StyledInput type="text" name="subject" id="subject" placeholder='Subject' required />
+        <StyledText name='message' id='message' placeholder='Message' required/>
+        <StyledButton type="submit">SEND</StyledButton>
+      </Form>
     </motion.div>
     </ContactStyle>
   )
--- src/styles/contactStyle.js
+++ src/styles/contactStyle.js
@@ -1,29 +1,83 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+
+
 const ContactStyle = styled(motion.div)`
   padding: 4rem 9rem;
   color:#353535;
   min-height: 90vh;
 `;
 const Title = styled.div`
-  margin-bottom: 4rem;
+  margin-bottom: 3rem;
   color: black;
 
 `;
 const Hide=styled.div`
   overflow: hidden;
+  text-align: center;
 `;
-const Circle = styled.div`
-  border-radius: 50%;
-  width: 3rem;
-  height: 3rem;
-  background: #353535;
+const Form = styled.form` 
+  margin: 3rem 5rem;
+  #name{
+    width: 49%;
+  }
+  #email{
+    margin-left: 1rem;
+    width: 49%;
+    
+  }
+  #subject{
+    width: 100%;    
+  }
+  @media(max-width: 576px){
+    margin: 3rem 3rem;
+    #name{
+      width: 47%;
+    }
+    #email{
+      width: 47%;
+    }
+  }
+  `;
+const StyledInput = styled.input`
+padding: 1rem;
+border: 1px solid #23d997;
+margin-top: 1.5rem;
+border-radius: 35px;
+&:hover{
+  border:1px solid #D96ED4;
+}
+  `;
+const StyledText = styled.textarea` 
+  width: 100%;
+  height: 15vh;
+  border: 1px solid #23d997;
+  border-radius: 15px;
+  margin-top: 1.5rem;
+  padding: 1rem;
+  &:hover{
+  border:1px solid #D96ED4;
+  font-family: "Lobster", cursive;
+  font-size: 1.5rem;
+  @media(max-width: 576px){
+    font-size: 1rem;
+  }
+}
 `;
-const Social = styled(motion.div)`
-  display: flex;
-  align-items: center;
-  h2{
-    margin: 2rem;
+const StyledButton = styled.button`
+  background-color: #23d997;
+  color: white;
+  padding: 1rem;
+  width: 30%;
+  float: right;
+  margin-top: 10px;
+  border: none;
+  border-radius: 10px;
+  font-size: 1.5rem;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  cursor: pointer;
+  &:hover{
+    background-color: #D96ED4;
   }
 `;
-export {ContactStyle, Title, Hide, Circle, Social}
+export {ContactStyle, Title, Hide, Form,StyledInput,StyledText, StyledButton}
