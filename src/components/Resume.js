6be37df1325bebcc34a67d4b8695983438489405
awaissaddiqui <awaissaddiqui143@gmail.com>
2023-08-13 01:12:34 +0500

    fix Downloading bug

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,9 @@ import styled from 'styled-components'
 const Resume = () => {
   return (
   <>
-  <Button>Download CV</Button>
+  <Button>
+     <a href='Awais.pdf' download="Awais.pdf">Download CV</a>     
+     </Button>
   
   </>
   )
@@ -13,9 +15,14 @@ const Button = styled.button`
      margin:4rem 10rem;
      border: 3px solid #23d997;
      padding: 1rem 1.5rem;
+     a{
+          color: white;
+          text-decoration: none;
+     }
      @media(max-width: 650px){
           padding: 1rem 8rem;
           margin:6rem 23rem;
      }
+
 `;
 export default Resume
