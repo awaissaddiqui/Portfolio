7f260b20b67695bfd5d239bb088769f60132a4e1
awaissaddiqui <awaissaddiqui143@gmail.com>
2023-09-04 17:10:03 +0500

    Personal detail added

--- src/components/HideShow.jsx
+++ src/components/HideShow.jsx
@@ -1,42 +1,50 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { Link } from 'react-router-dom'
-import styled from 'styled-components'
+import styled from 'styled-components';
+// Animations
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
 // import svg icons 
 import cv from "../images/assets/cv.svg"
+import academic from "../images/assets/academic.svg";
+import certificate from "../images/assets/certificate.svg";
+
 const Hide_show = () => {
+     useEffect(()=>{
+          AOS.init();
+     },[])
   return (
      <>
-    {/* <Personal>
-     <ul>
-          <li><Link to="/resume" ><Resume className="cv">Resume</Resume></Link></li>
-          <li><Link to="/academic" ><Academic className="academic">Academic Record</Academic></Link></li>
-          <li><Link to="/certificates" ><Certificate className="certificate">Achievements</Certificate></Link></li>
-     </ul>
-    </Personal> */}
     {/* Cards  */}
-    <div className="card-group mb-5">
-    <Data className="card col-sm-12bg-transparent border-success " >
+    <Heading  data-aos="zoom-in-up">
+    <h1>Welcome To my Portfolio</h1>
+    </Heading>
+    <CardGroup 
+    className="card-group text-white " 
+    data-aos="zoom-in-up"
+     data-aos-easing="ease-out-cubic"
+     data-aos-duration="1500"
+    >
+    <Data className="card col-sm-12  bg-info " >
   <img className="" src={cv} alt="Card "/>
   <div className="card-body ">
-    <h5 className="card-title">Card title</h5>
-     <Link to="#" className="btn btn-primary">Go somewhere</Link>
+     <Link to="/resume" className=""><button className="">View Resume</button></Link>
   </div>
 </Data>
-    <Data className="card col-sm-12" >
-  <img className="" src={cv} alt="Card  2"/>
+    <Data className="card col-sm-12 bg-danger" >
+  <img className="" src={academic} alt="Card  2"/>
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-     <Link to="#" className="btn btn-primary">Go somewhere</Link>
+     <Link to="/academic" className=""> <button className="">Acdemic Record</button></Link>
   </div>
 </Data>
-    <Data className="card col-sm-12" >
-  <img className="" src={cv} alt="Card  3"/>
+    <Data className="card col-sm-12 bg-success" >
+  <img className="" src={certificate} alt="Card  3"/>
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-     <Link to="#" className="btn btn-primary">Go somewhere</Link>
+     <Link to="/certificates" className=""><button className="">Achievements</button> </Link>
   </div>
 </Data>
-</div>
+</CardGroup>
 
      </>
   )
@@ -44,48 +52,31 @@ const Hide_show = () => {
 
 export default Hide_show
 
-// const Personal = styled.div` 
-//      ul{
-//           display: block;
-//           position: relative;
-//           list-style: none;
-//           padding: 5rem 0rem;
-//           margin: 2rem 0rem;
-//           width:12rem;
-//      }
-     
-//      li{
-//            margin: 1rem 0rem;
-//      }
-//      a{
-//           text-decoration: none;
-//      }
-//      `;
-
-// const Resume = styled.div`
-//      color: white;
-//      font-size: 1.5rem;
-//      background-color: #23d997;
-// `;
-// const Academic = styled.div`
-//      color: white;
-//      font-size: 1.5rem;
-//      background: #8ed2ff;
-// `;
-// const Certificate = styled.div`
-//      color: white;
-//      font-size: 1.5rem;
-//      background: #8effa0;
-// `;
+const CardGroup = styled.div`
+     min-height: 80vh;
+     width: 100%;
+     display: flex;
+     justify-content: center;
+     align-items: center;
+     @media(max-width: 768px) {
+          flex-direction:column ;
+          margin: 10rem 0rem;
+     }
+`
 const Data = styled.div`
      height: 25vh;
      text-align: center;
-     margin: 5rem 5.5rem;
-     padding-bottom: 4rem;
-
+     margin: 0rem 5rem;
+     padding: 1rem 0rem;
      img{
           width: 20%;
           height: 20vh;
-          margin: 1rem 7rem;
+          margin: 0.5rem 7.5rem;
      }
+
+`
+const Heading = styled.h1`
+     color: white;
+     font-family: "Inter", sans-serif;
+     text-align: center;
 `
